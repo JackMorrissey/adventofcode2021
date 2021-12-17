a8195cf386b96de6d2a411fef47f0483da04bf6c
Jack Morrissey <jack@tryce.tech>
2021-12-17 17:55:06 -0600

    feat(15): before caching refactor
    I don't know why I didn't learn from day 14

--- 15/index.mjs
+++ 15/index.mjs
@@ -1,4 +1,4 @@
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
 
 const failedPath = Number.MAX_SAFE_INTEGER;
 
@@ -17,30 +17,36 @@ function getSimplePathRisk(numberLines) {
 function getValidPositions(atRowI, atColJ, positions, visitedPositions) {
   const validPositions = [];
 
-  if (atRowI < positions.length - 1 && !visitedPositions.has(`${atRowI + 1}-${atColJ}`)) {
+  if (
+    atRowI < positions.length - 1 &&
+    !visitedPositions.has(`${atRowI + 1}-${atColJ}`)
+  ) {
     validPositions.push({
       i: atRowI + 1,
       j: atColJ,
     });
   }
-  if (atColJ < positions[0].length - 1 && !visitedPositions.has(`${atRowI}-${atColJ + 1}`)) {
+  if (
+    atColJ < positions[0].length - 1 &&
+    !visitedPositions.has(`${atRowI}-${atColJ + 1}`)
+  ) {
     validPositions.push({
       i: atRowI,
       j: atColJ + 1,
     });
   }
-  if (atColJ > 1 && !visitedPositions.has(`${atRowI}-${atColJ - 1}`)) {
-    validPositions.push({
-      i: atRowI,
-      j: atColJ - 1,
-    });
-  }
-  if (atRowI > 1 && !visitedPositions.has(`${atRowI - 1}-${atColJ}`)) {
-    validPositions.push({
-      i: atRowI - 1,
-      j: atColJ,
-    });
-  }
+  // if (atColJ > 1 && !visitedPositions.has(`${atRowI}-${atColJ - 1}`)) {
+  //   validPositions.push({
+  //     i: atRowI,
+  //     j: atColJ - 1,
+  //   });
+  // }
+  // if (atRowI > 1 && !visitedPositions.has(`${atRowI - 1}-${atColJ}`)) {
+  //   validPositions.push({
+  //     i: atRowI - 1,
+  //     j: atColJ,
+  //   });
+  // }
   return validPositions;
 }
 
@@ -50,7 +56,7 @@ function traverseForRisk(
   positions,
   visitedPositions,
   currentRisk,
-  toBeatRisk,
+  toBeatRisk
 ) {
   if (atRowI === positions.length - 1 && atColJ === positions[0].length - 1) {
     return currentRisk;
@@ -60,66 +66,87 @@ function traverseForRisk(
     return failedPath;
   }
 
-  const validPaths = getValidPositions(atRowI, atColJ, positions, visitedPositions);
+  const validPaths = getValidPositions(
+    atRowI,
+    atColJ,
+    positions,
+    visitedPositions
+  );
   if (!validPaths.length) {
     return failedPath;
   }
 
   let currentBestRisk = toBeatRisk;
-  validPaths.forEach((p) => {
+  for (let k = 0; k < validPaths.length; k++) {
+    const p = validPaths[k];
+    const nextRisk = currentRisk + positions[p.i][p.j];
+    if (nextRisk > currentBestRisk) {
+      continue;
+    }
     const nextVisited = new Set(visitedPositions);
     nextVisited.add(`${p.i}-${p.j}`);
-    const nextRisk = currentRisk + positions[p.i][p.j];
-    const resultingRisk = traverseForRisk(p.i, p.j, positions, nextVisited, nextRisk, currentBestRisk);
+    const resultingRisk = traverseForRisk(
+      p.i,
+      p.j,
+      positions,
+      nextVisited,
+      nextRisk,
+      currentBestRisk
+    );
     if (resultingRisk < currentBestRisk) {
       currentBestRisk = resultingRisk;
     }
-  });
+  }
   return currentBestRisk;
 }
 
 function partOne(positions) {
   const simpleRisk = getSimplePathRisk(positions);
-  const lowestRisk = traverseForRisk(0, 0, positions, new Set(['0-0']), 0, simpleRisk);
+  const lowestRisk = traverseForRisk(
+    0,
+    0,
+    positions,
+    new Set(["0-0"]),
+    0,
+    simpleRisk
+  );
   console.log(lowestRisk);
-};
-
-function partTwo(polymerTemplate, rules, depth) {
-
 }
+
+function partTwo(polymerTemplate, rules, depth) {}
 
 function getSampleLines() {
   return [
-    '1163751742',
-    '1381373672',
-    '2136511328',
-    '3694931569',
-    '7463417111',
-    '1319128137',
-    '1359912421',
-    '3125421639',
-    '1293138521',
-    '2311944581',
+    "1163751742",
+    "1381373672",
+    "2136511328",
+    "3694931569",
+    "7463417111",
+    "1319128137",
+    "1359912421",
+    "3125421639",
+    "1293138521",
+    "2311944581",
   ];
 }
 
 async function main() {
-  const contents = await readFile('input.txt', 'utf8');
-  const lines = contents.split('\n');
+  const contents = await readFile("input.txt", "utf8");
+  const lines = contents.split("\n");
   const numberLines = lines
-  // const numberLines = getSampleLines()
+    // const numberLines = getSampleLines()
     .filter((l) => !!l)
-    .map((l) => l.split('').map((c) => parseInt(c, 10)));
+    .map((l) => l.split("").map((c) => parseInt(c, 10)));
   partOne(numberLines);
   // partTwo(numberLines);
 }
 
 main().then(
   () => {
-    console.log('Done!');
+    console.log("Done!");
   },
   (e) => {
     console.error(e);
     console.error(e.stack);
-  },
+  }
 );
